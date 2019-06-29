40d9a8e5fa7a2405899ac25fe1208f5ba6ab8e0c
Balint Haui <balint.haui@gmail.com>
2019-06-29 18:38:05 +0200

    update--update first

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -10,12 +10,13 @@ const mongoDatabase = process.env.mongoDatabase;
 // use schema to define the shape of documents withing a collection in mongoDB
 
 
-
+// connect using mongoose
 mongoose.connect(`mongodb://${mongoDbServer}/${mongoDatabase}`, { useNewUrlParser: true }) //connect method, returns a promise
   .then(() => console.log('OK...connected to mongoDB!'))
   .catch(err => console.log('ERROR...connecting to mongoDB!', err));
 
-// schema, defines the shape of documents in a mongoDB database
+// schema, defines the shape of documents in a mongoDB database.
+// create schema
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
@@ -32,7 +33,15 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
+// Course = Collection (table in SQL) = Class
+// course = document (row/record in SQL) = object
 
+
+
+
+
+
+// add document to a collection 
 async function createCourse() {
   const course = new Course({
     name: 'Angular Course',
@@ -44,14 +53,13 @@ async function createCourse() {
   const result = await course.save(); // course here is a document(object), the save method returns a promise
   console.log(result);
 }
-
 //createCourse();
 
 
 
 
 // querying documents with collection methods chain
-async function getCourses(){
+async function getCourses() {
   //
   // COMPARISON OPERATORS!
   //
@@ -74,23 +82,113 @@ async function getCourses(){
   // /api/courses?pageNumber=2&pageSize=10
 
   const courses = await Course
-  
-  //.find({ price: { $gte: 10, $lte: 20 } }) // only where price is at least 10 but not higher than 20
-  //.find({ price: {$in: [10, 15, 20]} }) // 10 or 15 or 20
-  //.find({author: 'Mosh', isPublished: true}) // filter the resulting documents
-  .find()
-  .or([  {author: 'Mosh'}, {isPublished: true}  ])  // filter objects! the .find() method ahead of .or() is empty! Result: documents where author is Mosh OR it is published
-  .skip((pageNumber - 1) * pageSize) // pagination method, together with following .limit() method
-  .limit(pageSize) // .limit(10) <-- show 10 documents max.
-  .sort({name: 1})
-  .select({name: 1, tags: 1}); // only select the property you want from the document
+
+    //.find({ price: { $gte: 10, $lte: 20 } }) // only where price is at least 10 but not higher than 20
+    //.find({ price: {$in: [10, 15, 20]} }) // 10 or 15 or 20
+    .find({ author: 'Mosh', isPublished: true }) // filter the resulting documents
+    //.find()
+    //.or([  {author: 'Mosh'}, {isPublished: true}  ])  // filter objects! the .find() method ahead of .or() is empty! Result: documents where author is Mosh OR it is published
+    .skip((pageNumber - 1) * pageSize) // pagination method, together with following .limit() method
+    .limit(pageSize) // .limit(10) <-- show 10 documents max.
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 }); // only select the property you want from the document
   //.countDocuments(); // returns the number of docs. that match the above criteria
   console.log(courses);
 }
 
 // you either use .countDocuments() OR .select()
 
-getCourses();
+//getCourses();
 
 
 
+
+
+
+
+// UPDATING a document - 2 approaches
+async function updateCourseQF(id) {
+  // approach: query first (QF)
+  // findById()
+  // modify its properties
+  // save
+
+
+  const course = await Course.findById(id);
+  if (!course) {
+    return console.log('there is no course with that id');
+  }
+
+  //course.isPublished = true;
+  //course.author = 'Another Author';
+
+  course.set({
+    isPublished: true,
+    author: 'Another Author'
+  });
+
+  const result = await course.save();
+  console.log(result);
+
+}
+
+//updateCourseQF('5d175ec39a28d921a8c4a8df');
+
+
+
+
+async function updateCourseUF(id) {
+  // USAGE: likes, upvotes/downvotes
+
+  // approach: update first (UF)
+  // update directly
+  // optionally - get the updated document
+
+  // mongodb UPDATE OPERATORS are needed for the collections .update() methods second paramater,
+  // that is an UPDATE OBJECT
+  // docs.mongodb.com/manual/reference/operator/update
+
+  const result = await Course.update({ _id: id }, {
+    $set: {
+      author: 'Mosh',
+      isPublished: false
+    }
+  }
+  ); //{ isPublished: false } -- you could update all those documents where isPublished is false
+
+
+  console.log(result); // result here is similar to OK Packet in MySQL
+
+}
+
+//updateCourseUF('5d175ec39a28d921a8c4a8df')
+
+
+
+
+// get the document that was updated
+async function updateCourseUF2(id) {
+
+
+  const course = await Course.findByIdAndUpdate(id, {
+    $set: {
+      author: 'Jason',
+      isPublished: false
+    }
+  }, { new: true }); // , { new: true } without this you will get the document back before the update process
+
+
+  // printing the resulting record (object) THIS IS THE DOCUMENT BEFORE THE UPDATE OPERATION
+  //
+  // if you need the updated document back you need to pass an option... { new: true }
+  //
+  //
+
+
+
+  console.log(course);
+
+
+}
+
+updateCourseUF2('5d175ec39a28d921a8c4a8df')
